7a11d52702db38fa318ccb0e70fb1a1247fa5d03
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2017-02-09 22:04:43 -0500

    update

--- api-project/src/components/characters.js
+++ api-project/src/components/characters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Characters = (props) => (
+export const Characters = (props) => ( // Watch out for accidentally making this a function when you don't want it to be one.'
     <div> 
         <h4>This is the Characters app component.</h4>
     </div>
